66134d2dd09788f54d74d22964fd24e1e5f0ba47
imknown <imknown@qq.com>
2020-12-08 00:23:18 +0800

    Extract gradle configs and add some dependencies like Firebase, LeakCanary, etc. .

---
+++ app/src/debug/java/net/imknown/android/bundletooldevicespecjsongenerator/MyDebugApplication.kt
@@ -0,0 +1,18 @@
+package net.imknown.android.bundletooldevicespecjsongenerator
+
+import android.os.StrictMode
+import leakcanary.LeakCanaryProcess
+
+class MyDebugApplication : MyApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanaryProcess.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        StrictMode.enableDefaults()
+    }
+}
